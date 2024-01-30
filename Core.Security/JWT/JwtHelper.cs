234bd114ccb83ccf100b1564cd2c6d15dbd32331
Sinan Kara <snan_kara_@hotmail.com>
2024-01-30 20:25:13 +0300

    added classes for JWT and OTP Authenticator implementation.

---
+++ Core.Security/JWT/JwtHelper.cs
@@ -0,0 +1,85 @@
+﻿using Core.Security.Encryption;
+using Core.Security.Entities;
+using Core.Security.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.JWT;
+
+public class JwtHelper : ITokenHelper
+{
+    public IConfiguration Configuration { get; set; }
+    public readonly TokenOptions _tokenOptions;
+    public DateTime _accessTokenExpiration;
+
+    public JwtHelper(IConfiguration configuration)
+    {
+        Configuration = configuration;
+        const string configurationSection = "TokenOptions";
+        _tokenOptions = Configuration.GetSection(configurationSection).Get<TokenOptions>()
+            ?? throw new NullReferenceException($"\"{configurationSection}\" section cannot found in configuration");
+    }
+
+    public RefreshToken CreateRefreshToken(User user, string ipAddress)
+    {
+        RefreshToken refreshToken = new(user.Id, RandomRefreshToken(), DateTime.UtcNow.AddDays(7), ipAddress);
+
+        return refreshToken;
+    }
+
+    public AccessToken CreateToken(User user, IList<OperationClaim> operationClaims)
+    {
+        _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
+        SecurityKey securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+        SigningCredentials signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
+        JwtSecurityToken jwt = CreateJwtSecurityToken(_tokenOptions, user, signingCredentials, operationClaims);
+        JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+        string? token = jwtSecurityTokenHandler.WriteToken(jwt);
+
+        return new(token, _accessTokenExpiration);
+    }
+
+    public JwtSecurityToken CreateJwtSecurityToken(
+        TokenOptions tokenOptions,
+        User user,
+        SigningCredentials signingCredentials,
+        IList<OperationClaim> operationClaims)
+    {
+        return new JwtSecurityToken(
+            tokenOptions.Issuer,
+            tokenOptions.Audience,
+            expires: _accessTokenExpiration,
+            notBefore: DateTime.Now,
+            claims: SetClaims(user, operationClaims),
+            signingCredentials: signingCredentials
+            );
+    }
+
+    private IEnumerable<Claim> SetClaims(User user, IList<OperationClaim> operationClaims)
+    {
+        List<Claim> claims = new();
+
+        claims.AddNameIdentifier(user.Id.ToString());
+        claims.AddEmail(user.Email);
+        claims.AddName($"{user.FirstName} {user.LastName}");
+        claims.AddRoles(operationClaims.Select(x => x.Name).ToArray());
+
+        return claims;
+    }
+
+    private string RandomRefreshToken()
+    {
+        byte[] numberByte = new byte[32];
+        using var random = RandomNumberGenerator.Create();
+        random.GetBytes(numberByte);
+        return Convert.ToBase64String(numberByte);
+    }
+}
---
+++ Core.Security/JWT/TokenOptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.JWT;
+
+public class TokenOptions
+{
+    public string Audience { get; set; }
+    public string Issuer { get; set; }
+    public int AccessTokenExpiration { get; set; }
+    public string SecurityKey { get; set; }
+    public int RefreshTokenTTL { get; set; }
+
+    public TokenOptions()
+    {
+        
+    }
+
+    public TokenOptions(string audience, string ıssuer, int accessTokenExpiration, string securityKey, int refreshTokenTTL)
+    {
+        Audience = audience;
+        Issuer = ıssuer;
+        AccessTokenExpiration = accessTokenExpiration;
+        SecurityKey = securityKey;
+        RefreshTokenTTL = refreshTokenTTL;
+    }
+}
